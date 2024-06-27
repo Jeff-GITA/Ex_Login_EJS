21e2520ace9b7f8da48dad7ec00bd860378a3950
jeferson-gallo <jdgap9801@gmail.com>
2024-06-26 23:44:23 -0500

    Searching displays information

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain, net, ipcRenderer } = require('electron');
+const { app, BrowserWindow, ipcMain, net, ipcRenderer, screen } = require('electron');
 const path = require('path');
 const axios = require('axios');
 
@@ -6,7 +6,7 @@ const axios = require('axios');
 const API_URL = "https://5b9d91df-5241-4ae0-ab7d-6256341b374e.mock.pstmn.io"
 
 let mainWindow;
-let loginWindow;
+// let loginWindow;
 let username, password;
 
 
@@ -21,8 +21,8 @@ async function getUser(event, a, b) {
       }
     });
     console.log(response.data);
-    createMainWindow();
-    loginWindow.close();
+    showMainWindow();
+    // loginWindow.close();
 
   } catch (error) {
     if (error.response && error.response.status === 400) {
@@ -35,25 +35,10 @@ async function getUser(event, a, b) {
   }
 }
 
-function createLoginWindow() {
-  loginWindow = new BrowserWindow({
+function createLogin() {
+  mainWindow = new BrowserWindow({
     width: 400,
     height: 300,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: true,
-      enableRemoteModule: false,
-      // nodeIntegration: true,
-    },
-  });
-
-  loginWindow.loadFile('login.html');
-}
-
-function createMainWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
@@ -61,22 +46,49 @@ function createMainWindow() {
       nodeIntegration: false,
     },
   });
+  mainWindow.webContents.openDevTools();
+  mainWindow.loadFile('login.html');
+  display_Information();
+}
+
+function showMainWindow() {
+  // mainWindow = new BrowserWindow({
+  //   width: 800,
+  //   height: 600,
+  //   webPreferences: {
+  //     preload: path.join(__dirname, 'preload.js'),
+  //     contextIsolation: true,
+  //     enableRemoteModule: false,
+  //     nodeIntegration: false,
+  //   },
+  // });
 
   mainWindow.loadFile('mainWindow.html');
 
- 
   console.log("send credential...")
   mainWindow.webContents.send("send-credentials", {username, password});
   
   mainWindow.webContents.openDevTools();
+};
+
+function display_Information(){
+  // Retrieve information about all monitors.
+  const displays = screen.getAllDisplays();
+  const primaryDisplay = screen.getPrimaryDisplay()
+  const { width, height } = primaryDisplay.workAreaSize
+  console.log("Display information:")
+  console.log(displays);
+  console.log(`width:${width}, height:${height}`)
+  console.log("Number of displays: ",displays.length);
 }
 
 app.whenReady().then(() => {
-  createLoginWindow();
+  createLogin();
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createLoginWindow();
+      createLogin();
+      
     }
   });
 });
--- mainRenderer.js
+++ mainRenderer.js
@@ -1,8 +1,10 @@
+
 // This file can contain the logic for the main application window
 console.log('Main application window loaded');
 
 
 window.electronAPI.sendCredentials((event, data) => {
     const {username, password} = data;
+    console.log("Main window:")
     console.log("Credentials: ",username, password);
   });
